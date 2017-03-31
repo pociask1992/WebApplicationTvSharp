cdab4677ec095b960d3e3ba36f301adfad8f7e0c
pociask1992 <pociask1992@gmail.com>
2017-03-31 22:56:57 +0200

    Adding another files.

---
+++ WebApplicationTV/WebApplicationTV/Controllers/MoviesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplicationTV.Models;
+using WebApplicationTV.Services;
+
+namespace WebApplicationTV.Controllers
+{
+    public class MoviesController : ApiController
+    {
+        private MoviesServices _movieService;
+
+        public MoviesController()
+        {
+            _movieService = MoviesServices.Instance;
+        }
+
+        [HttpGet, Route("movies")]
+        public IHttpActionResult GetAllMovies()
+        {
+            List<Movie> movies = _movieService.GetAll();
+            return Ok(movies);
+        }
+
+        [HttpGet, Route("movies/{movieId:int}")]
+        public IHttpActionResult GetMovieById(int movieId)
+        {
+            Movie foundedMovie = _movieService.GetById(movieId);
+            if (foundedMovie == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(foundedMovie);
+        }
+
+        [HttpPost, Route("movies")]
+        public IHttpActionResult AddMovie([FromBody]Movie movie)
+        {
+            _movieService.AddNewMovie(movie);
+            return Ok();
+        }
+
+        [HttpDelete, Route("movies/{movieId:int}")]
+        public IHttpActionResult DeleteMovie(int movieId)
+        {
+            _movieService.DeleteMovie(movieId);
+            return Ok();
+        }
+    }
+}
